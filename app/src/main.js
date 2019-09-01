5b0452b351031056ab3aab64a2cb589dc14518c7
ZHAI-LIANG <1751242675@qq.com>
2019-09-01 18:36:35 +0800

    登陆成功页面

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "lib-flexible";
+import 'lib-flexible'
+import 'swiper/dist/css/swiper.css';
+import '../font/iconfont.css'
+
+
+
 
 Vue.config.productionTip = false;
 
