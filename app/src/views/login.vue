434c6f9d27470f4d9ff1ee6e447229492aa16758
ZHAI-LIANG <1751242675@qq.com>
2019-09-03 10:57:44 +0800

    单位更改1

--- app/src/views/login.vue
+++ app/src/views/login.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
 	    <header>
-		  	<i class="icon iconfont icon-sousuo" style="font-size: 0.24rem;float: left;line-height: 0.666666rem;margin-left: 0.133333rem;color: #fff;" @click="search"></i><p>上海<router-link to="./logon" class="j-router">
+		  	<i class="icon iconfont icon-sousuo" style="font-size: 0.276923rem;float: left;line-height: 0.76923rem;margin-left: 0.153846rem;color: #fff;" @click="search"></i><p>上海<router-link to="./logon" class="j-router">
 		  		<span v-show="false">登录|注册</span>
-		  		<span v-show="true"><i class="icon iconfont icon-rentou" style="font-size: 0.24rem;float: left;line-height: 0.666666rem;margin-right: 0.133333rem;color: #fff;"></i></span>
+		  		<span v-show="true"><i class="icon iconfont icon-rentou" style="font-size: 0.276923rem;float: left;line-height: 0.76923rem;margin-right: 0.153846rem;color: #fff;"></i></span>
 		  	</router-link></p>
 		</header>
 		
@@ -32,7 +32,7 @@
 		
 		<div class="main">
 			<div class="top">
-				<i class="icon iconfont icon-shangdian" style="font-size: 0.226666rem;margin-right: 0.053333rem;"></i>
+				<i class="icon iconfont icon-shangdian" style="font-size: 0.261538rem;margin-right: 0.061538rem;"></i>
 				附近商家
 			</div>
 			<div class="shops" v-for="i,key in allshop">
@@ -44,7 +44,7 @@
 					
 					<div class="talk">
 						评分
-						<div class="song" style="float: right;"><span>蜂鸟专送</span> <span style="color: #3190e8;border: 0.013333rem #3190e8 solid;background: #fff;">准时送</span></div>
+						<div class="song" style="float: right;"><span>蜂鸟专送</span> <span style="color: #3190e8;border: 0.015384rem #3190e8 solid;background: #fff;">准时送</span></div>
 					</div>
 					<div class="xian">
 						￥{{i.float_minimum_order_amount}}起送 / 配送费约 ￥{{i.float_delivery_fee}}
@@ -124,15 +124,15 @@
 *{padding: 0;margin: 0;}
 header{
 	width: 100%;
-	height: 1.333333rem;
+	height: 1.538461rem;
 	background: #3190e8;
 }
 header p{
-	height: 1.333333rem;
+	height: 1.538461rem;
 	color: #fff;
-	font-size: 0.4rem;
-	line-height: 1.333333rem;
-	padding: 0.066666rem 0.106666rem 0 0.106666rem;
+	font-size: 0.461538rem;
+	line-height: 1.538461rem;
+	padding: 0.076923rem 0.123076rem 0 0.123076rem;
 	box-sizing: border-box;
 }
 header span{
@@ -142,20 +142,20 @@ header span{
 
  .swiper-container {
     width: 100%;
-    height: 5.066666rem;
-    margin: 0.266666rem auto;
+    height: 5.846153rem;
+    margin: 0.307692rem auto;
 }
 .all{
 	width: 25%;
-	height: 2.133333rem;
-	padding: 0.08rem;
+	height: 2.461538rem;
+	padding: 0.092307rem;
 	box-sizing: border-box;
-	margin-bottom: 0.186666rem;
+	margin-bottom: 0.215384rem;
 	float: left;
 }
 .all>.icon{
-	width: 1.333333rem;
-	height: 1.333333rem;
+	width: 1.538461rem;
+	height: 1.538461rem;
 	margin-left: 20%;
 }
 .all>.icon>img{
@@ -165,26 +165,26 @@ header span{
 .main{
 	width: 100%;
 	height: auto;
-	margin-top: 0.4rem;
+	margin-top: 0.461538rem;
 }
 .main>.top{
-	height: 1.066666rem;
-	line-height: 1.066666rem;
+	height: 1.230769rem;
+	line-height: 1.230769rem;
 	padding-left: 1em;
 	text-align: left;
-	border-top: 0.013333rem #ccc solid;
-	border-bottom: 0.013333rem #ccc solid;
+	border-top: 0.015384rem #ccc solid;
+	border-bottom: 0.015384rem #ccc solid;
 }
 .main>.shops{
 	width: 100%;
-	height: 2.666666rem;
-	padding:0.24rem ;
+	height: 3.076923rem;
+	padding:0.276923rem ;
 	box-sizing: border-box;
-	border-bottom: 0.013333rem #ccc solid;
+	border-bottom: 0.015384rem #ccc solid;
 }
 .main>.shops>.shopimg{
-	width: 2rem;
-	height: 2rem;
+	width: 2.307692rem;
+	height: 2.307692rem;
 	float: left;
 }
 .main>.shops>.shopimg>img{
@@ -193,17 +193,17 @@ header span{
 }
 .main>.shops>.shoptext{
 	width: 76%;
-	height: 2rem;
+	height: 2.307692rem;
 	text-align: left;
 	float: right;
 }
 .main>.shops>.shoptext>div{
-	height: 0.666666rem;
+	height: 0.76923rem;
 }
 .main>.shops>.shoptext>.name>.shopname{
 	width: 50%;
-	height: 0.4rem;
-	line-height: 0.4rem;
+	height: 0.461538rem;
+	line-height: 0.461538rem;
 	float: left;
 	text-overflow: ellipsis;
 	white-space: normal;
@@ -211,12 +211,12 @@ header span{
 }
 .main>.shops>.shoptext>.name span{
 	display: inline-block;
-	font-size: 0.213333rem;
+	font-size: 0.246153rem;
 	float: left;
-	padding: 0.013333rem 0.053333rem;
+	padding: 0.015384rem 0.061538rem;
 	background: yellow;
-	margin-right: 0.053333rem;
-	border-radius: 0.053333rem;
+	margin-right: 0.061538rem;
+	border-radius: 0.061538rem;
 }
 .main>.shops>.shoptext>.talk>.song{
 	float: right;
@@ -224,11 +224,11 @@ header span{
 }
 .main>.shops>.shoptext>.talk>.song>span{
 	display: inline-block;
-	font-size: 0.186666rem;
-	padding: 0.013333rem 0.04rem;
+	font-size: 0.215384rem;
+	padding: 0.015384rem 0.046153rem;
 	color: #fff;
-	margin-right: 0.026666rem;
-	border-radius: 0.053333rem;
+	margin-right: 0.030769rem;
+	border-radius: 0.061538rem;
 	background: #3190e8;
 }
 </style>
